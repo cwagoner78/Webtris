68faa8c7fa47723f09861b07d88bb8c6efcbf5e3
ClintWagoner <cwagoner78@gmail.com>
2026-09-03 17:40:59 -0500

    WIP: render logic work

--- webtris.js
+++ webtris.js
@@ -1,10 +1,41 @@
+
+//grid rendering
 const grid = document.getElementById("grid");
 const gridSizeX = 10;
 const gridSizeY = 20;
+const squares = [];
 
-for (let i = 0; i < gridSizeX*gridSizeY; i++) 
-    {
+//blocks and shapes
+const blockShape_T = [
+    [1, 1, 1, 0],
+    [0, 1, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],]
+
+let activeBlock = blockShape_T;
+let activeBlockPosition = [0, 0]; //rows, columns
+let activeBlockRotation = 0;
+
+setInterval(() => {
+    console.log(activeBlock, activeBlockPosition, activeBlockRotation);
+    activeBlockPosition[0]++; //rows
+}, 1000);
+
+//create grid
+for (let row = 0; row < gridSizeY; row++){
+    squares.push([]); //start new row
+    for (let col = 0; col < gridSizeX; col++){
         const square = document.createElement("div");
         square.classList.add("square");
         grid.appendChild(square);
+        squares[row].push(square);
     }
+    
+for (let blockPiece = 0; blockPiece < blockShape_T; i++){
+     //if blockPiece > 0 (do the thing);
+
+}
+
+
+
+}
