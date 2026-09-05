3d9e4daf463d5d08ca35d986146b55e03c4f1618
ClintWagoner <cwagoner78@gmail.com>
2026-09-04 22:10:41 -0500

    Tetromino rotation algo

--- webtris.js
+++ webtris.js
@@ -9,45 +9,28 @@ const gridSizeX = 10;
 const gridSizeY = 20;
 const squares = [];
 
+
+
 //blocks and shapes
+let blockSize = 3;
 const blockShape_T = [
-    [1, 1, 1, 0],
-    [0, 1, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],]
+    [0, 1, 0],
+    [1, 1, 1],
+    [0, 0, 0],
+    ]
 
 let activeBlock = blockShape_T;
 let activeBlockPosition = [0, 0]; //rows, columns
-let activeBlockRotation = 0;
+
 
 //User input
-let moveBlockLeft = addEventListener("keydown", event => {if (event.code==="KeyA") activeBlockPosition[1]--;}) 
-let moveBlockRight = addEventListener("keydown", event => {if (event.code==="KeyD") activeBlockPosition[1]++;})   
-
-
+addEventListener("keydown", event => {ControlBock(event, blockSize)}) 
+ 
+//Tick - game speed
 setInterval(() => {
-    console.log(activeBlock, activeBlockPosition, activeBlockRotation);
+    //console.log(activeBlock, activeBlockPosition);
     activeBlockPosition[0]++; //rows
-
-    //remove filled squares before rendering new blocks
-    for (let row = 0; row < gridSizeY; row++){
-        for (let col = 0; col < gridSizeX; col++){
-            squares[row][col].classList.remove("filled");
-        }
-    }
-
-    //render block position and fill squares
-    for (let localRow = 0; localRow < 4; localRow++){
-        for (let localCol = 0; localCol < 4; localCol++){
-            let currentPosX = activeBlockPosition[0] + localRow;
-            let currentPosY = activeBlockPosition[1] + localCol;
-            console.log("Active Block Position: ", currentPosX, currentPosY);
-    
-            if (blockShape_T[localRow][localCol] == 1){
-                squares[currentPosX][currentPosY].classList.add("filled");      
-            } 
-        }
-    }
+    UpdateBlock();
 }, gameSpeed);
 
 //create grid
@@ -59,5 +42,70 @@ for (let row = 0; row < gridSizeY; row++){
         grid.appendChild(square);
         squares[row].push(square);
     }
+}
+
+function UpdateBlock ()
+{
+    //remove filled squares before rendering new blocks
+    for (let row = 0; row < gridSizeY; row++){
+        for (let col = 0; col < gridSizeX; col++){
+            squares[row][col].classList.remove("filled");
+        }
+    }
+
+    //render squares
+    for (let localRow = 0; localRow < blockSize; localRow++){
+        for (let localCol = 0; localCol < blockSize; localCol++){
+            let currentPosX = activeBlockPosition[0] + localRow;
+            let currentPosY = activeBlockPosition[1] + localCol;
+            //console.log("Active Block Position: ", currentPosX, currentPosY);
+            if (activeBlock[localRow][localCol] == 1){
+                squares[currentPosX][currentPosY].classList.add("filled");      
+            } 
+        }
+    }
+}
+
+function ControlBock(event, size){
+    if (event.code==="KeyA"||event.code==="ArrowLeft"){
+        activeBlockPosition[1]--;UpdateBlock();
+    } else if (event.code==="KeyD"||event.code==="ArrowRight") {
+        activeBlockPosition[1]++;UpdateBlock();
+    } else if (event.code==="KeyW"||event.code==="ArrowUp") {
+        activeBlock = RotateBlock(activeBlock, 0, size);
+        UpdateBlock();
+    } else if (event.code==="KeyS"||event.code==="ArrowDown") {
+        activeBlock = RotateBlock(activeBlock, 1, size);
+        UpdateBlock();
+        
+    }
+}
+
+function RotateBlock(activeBlock, direction, size){
+    let rotateBlock = Array(size).fill(0).map(x => Array(size).fill(0));
+    for (let row = 0; row < size; row++){
+        for (let col = 0; col < size; col++){
+            
+            //store old rotation
+            let old = activeBlock[row][col];
+            
+            //clockwise
+            if (direction == 1){
+                let newRowClock = col;    
+                let newColClock = (size - 1) - row;
+                rotateBlock[newRowClock][newColClock] = old;
+            }
+            
+            //counter
+            if (direction == 0){
+                let newRowCounter = (size - 1) - col;
+                let newColCounter = row;
+                rotateBlock[newRowCounter][newColCounter] = old;
+            }
+
+           
+        }
+    }
+    return rotateBlock;
 }
 
